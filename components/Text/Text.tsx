913cd0f73d459719e7434e4addb4b91345dbba8b
Gromova Maria <masia86@mail.ru>
2024-03-22 10:30:05 +0300

    добавила в компонент Text использование шрифта определенного в textFont

--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -1,9 +1,10 @@
 import { TextProps } from "./Text.props";
 import styles from "./Text.module.css";
 import cn from "classnames";
+import { textFont } from "@/fonts/fonts";
 
 export const Text = ({ className, children, ...props }: TextProps): JSX.Element => {
-    return <p className={cn(styles.text, className)}
+    return <p className={cn(textFont.className, styles.text, className)}
         {...props}>
         {children}
     </p>;
