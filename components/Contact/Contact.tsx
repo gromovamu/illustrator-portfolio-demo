89a07e9bef7acdf0600bc6b20a47e45b37aaee1b
Gromova Maria <masia86@mail.ru>
2024-04-01 14:35:48 +0300

    добавлена картинка в компонент контакты.

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -4,22 +4,31 @@ import styles from "./Contact.module.css";
 import cn from "classnames";
 import { Htag, InlineLink, Text } from "@/components";
 import { saveRefByID } from "@/utils/scrollById";
+import Image from 'next/image';
 
 export const Contact = ({ mail, telegram, className, ...props }: ContactProps): JSX.Element => {
- 
+
   return (
     <section id="contacts" className={cn(styles.contact, className)} ref={node => saveRefByID("contacts", node)} {...props}>
       <Htag tag="h2" opt="medium" className={styles.title}>Контакты</Htag>
       <Text className={styles.text}>
         Вы&nbsp;можете написать мне в&nbsp;телеграм&nbsp;
-        <InlineLink decor="back" href={telegram.href } target="_blank">
-        {telegram.text}
+        <InlineLink decor="back" href={telegram.href} target="_blank">
+          {telegram.text}
         </InlineLink>
         &nbsp;или на&nbsp;электронную почту &nbsp;
         <InlineLink decor="back" href={mail.href}>
-       {mail.text}
+          {mail.text}
         </InlineLink>
-      </Text>
+      </Text>       
+      <div className={styles.imgContainer}>
+        <Image className={styles.img}
+          fill
+          priority={true}
+          src='img/main/contacts.svg'
+          alt='Декоративное изображение почтового ящика' />
+      </div>
+
     </section>);
 };
 
