6dd9a7263243459c79fb833f5e4f0f42b6fbe9d8
Gromova Maria <masia86@mail.ru>
2024-06-25 11:57:10 +0300

    переделала слайдер, чтобы не было видно обложек пока не запустится

--- components/CoverSlider/CoverSlider.tsx
+++ components/CoverSlider/CoverSlider.tsx
@@ -8,14 +8,15 @@ import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
 import { ArrowButton } from "@/components";
 import ExportedImage from "next-image-export-optimizer";
-
+import { useState } from "react";
 
 export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps): JSX.Element => {
-  // const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
     loop: true,
-    mode: "snap",
+    mode: "free-snap",  
     breakpoints: {
       "(min-width: 520px)": {
         slides: { perView: 3, spacing: 16 },
@@ -31,7 +32,10 @@ export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps
       },
     },
     slides: { perView: 2, spacing: 16 },
-  });
+    created() {
+      setLoaded(true);      
+    }
+  }); 
 
   return (
     <div className={cn("slider-container", styles.container, className)} {...props}>
@@ -40,13 +44,13 @@ export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps
           opt="left"
           onClick={() => instanceRef.current?.prev()}
         />
-
         <div ref={sliderRef} className={cn("keen-slider", styles.slider)}>
           {coverList && coverList.map((cover) => (
-            <div key={`cMi_${cover.id}`} className={cn("keen-slider__slide", styles.cover)}>
+            <div key={`cMi_${cover.id}`} className={cn("keen-slider__slide", styles.cover,
+              { [styles.hidden]: !loaded })}>
               <ExportedImage className={styles.img}
                 width={210}
-                height={305}                
+                height={305}
                 src={cover.url}
                 alt='' />
             </div>
@@ -64,3 +68,21 @@ export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps
 
 // При генерации обложек ссылок используется идентификатор обложки,
 //TODO: не могу решить как задавать отстуа для стрелок слайдера, пока сделан абсолютно через px
+
+
+/* анимация для слайдера, все работает, но мне почему-то не нравится
+оставляю на случай, если передумаю
+
+  const animation = { duration: 40000, easing: (t: number) => t };
+  ..
+    drag: false,
+created(s) {
+  setLoaded(true);
+  s.moveToIdx(5, true, animation);
+},
+updated(s) {
+  s.moveToIdx(s.track.details.abs + 5, true, animation);
+},
+animationEnded(s) {
+  s.moveToIdx(s.track.details.abs + 5, true, animation);
+},*/
