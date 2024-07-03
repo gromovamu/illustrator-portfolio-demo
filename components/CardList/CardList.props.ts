9f94306c8265acc21ad3afe7630cffd4e01af1f2
Gromova Maria <masia86@mail.ru>
2024-07-03 13:56:29 +0300

    добавлена анимация для появления карточек иллюстраций

--- components/CardList/CardList.props.ts
+++ components/CardList/CardList.props.ts
@@ -1,8 +1,15 @@
 import { Illustration } from "@/interfaces";
-import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
 export interface CardListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> { 
     cardList: Illustration[];
     seriaDecor: true|false;
+}
+
+export interface CardItemProps extends DetailedHTMLProps<HTMLAttributes<HTMLLIElement>, HTMLLIElement> { 
+    index: number;
+    isCanOpen: boolean;
+    handleSetCanOpen: (i: number) => void;
+    children: ReactNode;
 }
 
--- components/CardList/CardList.tsx
+++ components/CardList/CardList.tsx
@@ -1,24 +1,88 @@
 'use client';
-import { CardListProps } from "./CardList.props";
+import { CardItemProps, CardListProps } from "./CardList.props";
 import styles from "./CardList.module.css";
 import cn from "classnames";
 import { Card } from "@/components";
+import { useInView } from "react-intersection-observer";
+import { animated, easings, useSpring, useSpringRef } from "@react-spring/web";
+import { useEffect, useState } from "react";
 
 export const CardList = ({ seriaDecor, cardList, className, ...props }: CardListProps): JSX.Element => {
-   return (
+  const [isCanOpenArr, setIsCanOpenArr] = useState(Array.from(Array(cardList.length).keys(), (i) => i === 0 ? true : false));
+  
+  const handleAddCanOpen = (i: number): void => {
+    setIsCanOpenArr(arr => { arr.splice(i, 1, true); return [...arr] });
+    console.log(`handleAddCanOpen ${i}`);
+  };
+
+  return (
     <ul className={cn(styles.list, className)} {...props}>
       {
-        cardList && cardList.map(card => {
-          const isSeria = card.seriaId == null ? false : true;
+        cardList && cardList.map((card,i) => {
+          const isSeria = card.seriaId === null ? false : true;
           return (
-          <li className={cn(styles.item,  {
-            [styles.decor] : isSeria&&seriaDecor
-          })} key={card.id}>
-            <Card title={card.title} seria={isSeria} src={card.url} href={card.href??''} />
-          </li>
-          ); 
-        }) 
+            <CardItem className={cn(styles.item, {
+              [styles.decor]: isSeria && seriaDecor
+            })} key={`il_c_${card.id}`}
+              index={i}
+              isCanOpen={isCanOpenArr[i]}
+              handleSetCanOpen={handleAddCanOpen}>
+              <Card title={card.title} seria={isSeria} src={card.url} href={card.href ?? ''} />
+            </CardItem>
+          );
+        })
       }
     </ul>
   );
 };
+
+
+function CardItem({ index, isCanOpen, handleSetCanOpen, children, ...props }: CardItemProps): JSX.Element {
+  const api = useSpringRef();
+  const [isInView, setIsInView] = useState(false);
+  const style = useSpring(
+    {
+      ref: api,
+      from: { opacity: 0, scale: 0 },
+      config: {
+        duration: 1,
+      },
+    }
+  );
+
+  const [ref] = useInView({
+    /* Optional options */
+    threshold: 1,
+    triggerOnce: true,
+    fallbackInView: true,
+    onChange: (inView) => {
+      console.log(inView);
+      if (inView) {
+        setIsInView(true);
+      }
+    }
+  });
+
+  useEffect(() => {
+    console.log(`useEffect in card`);
+    if (isCanOpen && isInView) {
+      api.start({
+        to: { opacity: 1, scale: 1, },
+        delay: index == 0 ? 0 : 100,
+        config: {
+          duration: 300,
+          easing: easings.easeOutBack
+        },
+        onStart: () => {
+          handleSetCanOpen(index + 1);
+        }
+      });
+    }
+
+    return;
+  }, [isCanOpen, isInView, api, index, handleSetCanOpen]);
+
+  return <animated.li ref={ref } className={styles.item} style={style} {...props}>{children}</animated.li>;
+}
+
+// TODO: разобраться как помирить ref из inView и animate
