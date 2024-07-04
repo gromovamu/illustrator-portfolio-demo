4306e238c456adf9ce82f446da472a80213de613
Gromova Maria <masia86@mail.ru>
2024-07-04 14:09:51 +0300

    добавила анимацию стлайдера при наведении курсора

--- components/CoverSlider/CoverSlider.tsx
+++ components/CoverSlider/CoverSlider.tsx
@@ -10,13 +10,17 @@ import { ArrowButton } from "@/components";
 import ExportedImage from "next-image-export-optimizer";
 import { useState } from "react";
 
+const animation = { duration: 10000, easing: (t: number) => t };
+
+
 export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps): JSX.Element => {
   const [loaded, setLoaded] = useState(false);
+  const [isHover, setIsHover] = useState(false);
 
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
     loop: true,
-    mode: "free-snap",  
+    mode: "free-snap",
     breakpoints: {
       "(min-width: 520px)": {
         slides: { perView: 3, spacing: 16 },
@@ -33,18 +37,31 @@ export const CoverSlider = ({ coverList, className, ...props }: CoverSliderProps
     },
     slides: { perView: 2, spacing: 16 },
     created() {
-      setLoaded(true);      
-    }
-  }); 
+      setLoaded(true);
+    },
+    animationEnded(ref) {
+      isHover && ref.moveToIdx(ref.track.details.abs + 5, true, animation);
+    },
+  });
+
 
   return (
-    <div className={cn("slider-container", styles.container, className)} {...props}>
+    <div className={cn("slider-container", styles.container, className)}
+      {...props}>
       <div className={cn("navigation-wrapper", styles.wrapper)}>
         <ArrowButton className={cn(styles.arrow, styles.left)}
           opt="left"
           onClick={() => instanceRef.current?.prev()}
         />
-        <div ref={sliderRef} className={cn("keen-slider", styles.slider)}>
+        <div ref={sliderRef} className={cn("keen-slider", styles.slider)}
+          onMouseEnter={() => {
+            instanceRef.current?.moveToIdx(instanceRef.current.track.details.abs + 5, true, animation);
+            setIsHover(true);
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+            instanceRef.current?.animator.stop();
+          }}>
           {coverList && coverList.map((cover) => (
             <div key={`cMi_${cover.id}`} className={cn("keen-slider__slide", styles.cover,
               { [styles.hidden]: !loaded })}>
