6880a1da2a671ffc26975bd6e6389663ee960d3c
Gromova Maria <masia86@mail.ru>
2024-08-29 12:12:43 +0300

    компонет Серия разделен на 2 компонета, в зависимости от размера экрана, добавлен обработчик на resize, убран закоментированный кусок кода из IllistrationDetail

--- components/Seria/Seria.tsx
+++ components/Seria/Seria.tsx
@@ -10,11 +10,21 @@ import ExportedImage from "next-image-export-optimizer";
 
 export const Seria = ({ ...props }: SeriaProps): JSX.Element => {
   const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
+
+  const onResize = () => {
     setIsMobile(window.innerWidth <= 520);
+  };
+
+  useEffect(() => {
+    onResize();
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
   }, []);
-  return isMobile ? (<SeriaMobile {...props} />) :
-    (<SeriaDesktop {...props} />);
+
+  return (isMobile ? (<SeriaMobile  {...props} />) :
+    (<SeriaDesktop  {...props} />));
 };
 
 //------------------------------------------------------------------------------------------------
@@ -33,7 +43,7 @@ export const SeriaDesktop = ({ seria, illustrationDataList, className, ...props 
 
   const getAriaLabel = (img: IllustrationData) => {
     return `Увеличить иллюстрацию ${getPropertyInfoDetail('Название', img.details)}`;
-  }; 
+  };
 
   return (
     <div className={cn(styles.seria, className)} {...props}>
@@ -54,7 +64,7 @@ export const SeriaDesktop = ({ seria, illustrationDataList, className, ...props 
       </div>
 
       <Modal isOpen={isOpenModal} setIsOpen={setIsOpenModal}>
-        <div className={styles['modal-container']}>            
+        <div className={styles['modal-container']}>
           <Text className={styles.title}>{
             getPropertyInfoDetail('Название', illustrationDataList[activeImgIndex].details)
           } </Text>
@@ -77,10 +87,20 @@ export const SeriaDesktop = ({ seria, illustrationDataList, className, ...props 
 //------------------------------------------------------------------------------------------------
 const SeriaMobile = ({ seria, illustrationDataList, className, ...props }: SeriaProps): JSX.Element => {
   const [activeImgIndex, setActiveImgIndex] = useState(0);
-  const seriaDetail = getSeriaInfoDetails(seria);
+  const seriaDetail = getSeriaInfoDetails(seria); 
 
   const onClick = (activeImgNum: number) => {
-    setActiveImgIndex(activeImgNum + 1);
+    let temp = activeImgNum;
+    switch (activeImgNum) {
+      case -1: temp = 0; break;
+      case illustrationDataList.length: temp = 0; break;
+    }
+    setActiveImgIndex(temp);
+  };
+
+  const onClickImg = () => {
+    const next = activeImgIndex + 1;
+    setActiveImgIndex((next >= illustrationDataList.length) ? 0 : next);
   };
 
   const getAriaLabel = (img: IllustrationData) => {
@@ -91,30 +111,31 @@ const SeriaMobile = ({ seria, illustrationDataList, className, ...props }: Seria
     <div className={cn(styles.seria, className)} {...props}>
       <div className={styles.data}>
 
-        <IllustrationDetails data={seriaDetail} />
+        <IllustrationDetails data={[...seriaDetail, ...illustrationDataList[activeImgIndex].details]} />
 
-        <button className={cn('btn', styles['img-btn'])}>
-          <ExportedImage className={styles.img}
-            width={512}
-            height={512}
-            sizes="(max-width: 420px) 380px, 512px"
-            priority
-            src={illustrationDataList[activeImgIndex].url}
-            alt=''
-            aria-label='Следующая иллюстрация'
-          />
-        </button>
+        <div className={styles['img-btn-block']}>
+          <button className={cn('btn', styles['img-btn'])}
+            onClick={onClickImg}>
+            <ExportedImage className={styles.img}
+              width={512}
+              height={512}
+              sizes="(max-width: 420px) 380px, 512px"
+              priority
+              src={illustrationDataList[activeImgIndex].url}
+              alt=''
+              aria-label='Следующая иллюстрация'
+            />
+          </button>
 
-        <ArrowButton className={cn(styles.arrow, styles.left)}
-          opt="left"
-          onClick={() => onClick(activeImgIndex - 1)}
-          disabled={activeImgIndex === 0}
-          aria-label="Предыдущая иллюстрация" />
-        <ArrowButton className={cn(styles.arrow, styles.right)}
-          opt="right"
-          onClick={() => onClick(activeImgIndex + 1)}
-          disabled={activeImgIndex === (illustrationDataList.length - 1)}
-          aria-label="Следующая иллюстрация" />
+          <ArrowButton className={cn(styles.arrow, styles.left)}
+            opt="left"
+            onClick={() => onClick(activeImgIndex - 1)}
+            aria-label="Предыдущая иллюстрация" />
+          <ArrowButton className={cn(styles.arrow, styles.right)}
+            opt="right"
+            onClick={() => onClick(activeImgIndex + 1)}
+            aria-label="Следующая иллюстрация" />
+        </div>
       </div>
 
       <div className={styles['btn-container']}>
@@ -131,44 +152,3 @@ const SeriaMobile = ({ seria, illustrationDataList, className, ...props }: Seria
     </div>
   );
 };
-
-
-/*
-  <div className={styles.data}>
-        <IllustrationDetails data={getData(activeImgIndex)} nav={true}>
-          <ArrowButton className={cn(styles.arrow, styles.left)}
-            opt="left"
-            onClick={() => onClick(activeImgIndex - 1)}
-            disabled={activeImgIndex === 0}
-            aria-label="Предыдущая иллюстрация" />
-          <ArrowButton className={cn(styles.arrow, styles.right)}
-            opt="right"
-            onClick={() => onClick(activeImgIndex + 1)}
-            disabled={activeImgIndex === (illustrationDataList.length - 1)} 
-            aria-label="Следующая иллюстрация"/>
-        </IllustrationDetails>
-      </div>
-
-      <div className={styles.container}>
-        {illustrationDataList.map((img, i) => (
-          <ImgButton key={`${seria.id}_${i}`}
-            className={styles.card}
-            imgUrl={img.url}
-            alt={getPropertyInfoDetail('Название', img.details)}
-            onClick={() => onClick(i)}
-            aria-label={getAriaLabel(img)}
-            />
-        ))}
-      </div>
-
-      <div className={styles.btnContainer}>
-        {illustrationDataList.map((img, i) => (
-          <Button key={`btn_${seria.id}_${i}`}
-            className={cn(styles.btn, {
-              [
-              styles.active]: activeImgIndex === i
-            })}
-            aria-label={getAriaLabel(img)}
-            onClick={() => onClick(i)}>{i + 1}</Button>
-        ))}
-      </div>*/
